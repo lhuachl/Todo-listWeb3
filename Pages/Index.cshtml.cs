2abccdfd666374d71715878c9d4db369eae788f7
lhuachl <lllhuachlll@gmail.com>
2025-08-21 19:17:25 -0400

    Agregar archivos de proyecto.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Todo_listWeb3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Todo-listWeb3/Pages/TodoList.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Todo_listWeb3.Pages
+{
+    public class TodoListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
